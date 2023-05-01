7aea08d1cb626d093777e4949229527f39fe5dc7
mostafa <mostafadar489@gmail.com>
2023-05-01 14:22:57 +0300

    first commit

---
+++ Generate table of Big Bang episodes.sql
@@ -0,0 +1,17 @@
+create database Wistful;
+USE Wistful
+GO
+
+CREATE TABLE BigBangEpisodes(
+	SeasonNumber int NULL,
+	EpisodeNumber int NULL,
+	DateBroadcast date NULL,
+	Title varchar(255) NULL
+)
+
+GO
+
+-- show table
+SELECT * FROM BigBangEpisodes
+
+
